8c2c4b2110a9ccfa76727c28c0d5bc79f3dce584
Nick <baihesong@pku.edu.cn>
2016-06-07 14:50:23 +0800

    1

--- weixin_ws1.js
+++ weixin_ws1.js
@@ -78,65 +78,8 @@ app.get('/index',function(req,res){
     res.sendFile('/index.html',options);
 });
 app.listen(80);
-
 console.log("Weixin server runing at port: " + PORT + ".");
 
-var PORT = require('./lib/config').wxPort;
-var http = require('http');
-var qs = require('qs');
-
-var TOKEN = 'hahaha';
-
-function checkSignature(params, token){
-  //1. 将token、timestamp、nonce三个参数进行字典序排序
-  //2. 将三个参数字符串拼接成一个字符串进行sha1加密
-  //3. 开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
-
-  var key = [token, params.timestamp, params.nonce].sort().join('');
-  var sha1 = require('crypto').createHash('sha1');
-  sha1.update(key);
-
-  return  sha1.digest('hex') == params.signature;
-}
-
-
-var server = http.createServer(function (request, response) {
-
-  //解析URL中的query部分，用qs模块(npm install qs)将query解析成json
-  var query = require('url').parse(request.url).query;
-  var params = qs.parse(query);
-
-  if(!checkSignature(params, TOKEN)){
-    //如果签名不对，结束请求并返回
-    response.end('signature fail');
-    return;
-  }
-
-	if (request.method == 'GET') {
-		response.end(params.echostr);
-	}
-	else{
-		var postdata = "";
-
-		request.addListener("data",function(postchunk){
-			postdata += postchunk;
-		});
-
-		request.addListener("end",function(){
-			var parseString = require('xml2js').parseString;
-
-			parseString(postdata,function(err,result){
-				if (!err) {
-					var res = replyText(result);
-					response.end(res);
-				}
-			});
-		});
-	}
-});
-
-server.listen(PORT);
-console.log("Server is running at port:" + PORT + ".");
 
 function replyText(msg){
 /*  if(msg.xml.MsgType[0] !== 'text'){
