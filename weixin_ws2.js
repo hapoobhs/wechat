8b2910d642e51d922fbe078d5619313c7da841a2
Nick <baihesong@pku.edu.cn>
2016-06-07 09:57:05 +0800

    1

--- weixin_ws2.js
+++ weixin_ws2.js
@@ -70,16 +70,3 @@ var server = http.createServer(function (request, response) {
 server.listen(PORT);
 
 console.log("Weixin server runing at port: " + PORT + ".");
-
-var express = require('express');
-var app = express();
-app.get('/index',function(req,res){
-    var options = {
-        root:__dirname,
-        headers:{
-            'Upgrade':'websocket'
-        }
-    };
-    res.sendFile('/index.html',options);
-});
-app.listen(80);
